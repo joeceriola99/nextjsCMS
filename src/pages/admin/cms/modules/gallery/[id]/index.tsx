ac57c8284bae5245a12054d3363a84867a5e65a3
Elias David Ceriola <davejjceriola@gmail.com>
2021-04-29 20:48:43 +0800

    joe demo

--- src/pages/admin/cms/modules/gallery/[id]/index.tsx
+++ src/pages/admin/cms/modules/gallery/[id]/index.tsx
@@ -5,6 +5,7 @@ import { Card, CardBody } from '@paljs/ui/Card';
 
 import { Button as OldButton } from '@paljs/ui/Button';
 
+import { InputGroup } from '@paljs/ui/Input';
 import { EvaIcon } from '@paljs/ui/Icon';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
@@ -14,17 +15,20 @@ import * as Yup from 'yup';
 import Row from '@paljs/ui/Row';
 import Col from '@paljs/ui/Col';
 import { GrAddCircle } from 'react-icons/gr';
-import { db } from '../../../../../../../firebase';
+import { db, storage } from '../../../../../../../firebase';
 import { List, ListItem } from '@paljs/ui/List';
 import { FiEdit } from 'react-icons/fi';
 import { GrSubtractCircle } from 'react-icons/gr';
 import { Carousel } from 'react-responsive-carousel';
+import { IconContext } from 'react-icons';
 
 const Button = styled(OldButton)`
   text-transform: none !important;
 `;
+
 const Gallery = () => {
   const router: any = useRouter();
+  const [galleryImages, setGalleryImages] = useState([]);
   const initialValues = {
     name: '',
   };
@@ -40,7 +44,6 @@ const Gallery = () => {
   const { reset } = methods;
 
   const handleSubmit = (data: any) => {
-    console.log(data, 'DATA SUBMIT');
     db.collection('modules')
       .doc(router.query.id)
       .update({ name: data.name })
@@ -48,6 +51,25 @@ const Gallery = () => {
         router.push('/admin/cms/modules');
       });
   };
+
+  useEffect(() => {
+    const unsub = db
+      .collection('modules')
+      .doc(router.query.id)
+      .collection('data')
+      .onSnapshot((snapshot: any) => {
+        setGalleryImages(
+          snapshot.docs.map((doc: any) => {
+            return {
+              ...doc.data(),
+              id: doc.id,
+            };
+          }),
+        );
+      });
+
+    return unsub;
+  }, []);
 
   useEffect(() => {
     if (router.query.id) {
@@ -60,48 +82,94 @@ const Gallery = () => {
   }, [router.query.id]);
   return (
     <Layout title="Gallery">
-      <Form provider={methods} onSubmit={handleSubmit}>
-        <Card>
-          <CardBody>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-              <h5>Module Gallery</h5>
+      <Card>
+        <CardBody>
+          <Form provider={methods} onSubmit={handleSubmit}>
+            <>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <h5>Module Gallery</h5>
+
+                <Row>
+                  <Button status="Basic" onClick={() => router.back()}>
+                    Close
+                  </Button>
+                  <Button style={{ margin: '0px 10px' }} status="Basic">
+                    Save
+                  </Button>
+                </Row>
+              </div>
               <Row>
-                <Button status="Basic" onClick={() => router.back()}>
-                  Close
-                </Button>
-                <Button style={{ margin: '0px 10px' }} status="Basic">
-                  Save
-                </Button>
+                <Col breakPoint={{ xs: 12, sm: 4 }}>
+                  <Card size="Large" style={{ border: '2px solid rgb(237, 241, 247)', boxShadow: 'none' }}>
+                    <CardBody>
+                      <br />
+                      <Input name="name" placeholder="Gallery Title" />
+                      <List></List>
+                      <br />
+
+                      <InputGroup>
+                        <input
+                          type="file"
+                          className="custom-file-input"
+                          style={{ width: '100%' }}
+                          onChange={(e: any) => {
+                            console.log(e.target.files[0], 'TARGET FILES');
+                            const storageRef = storage.ref('/carousels/' + e.target.files[0].name);
+                            storageRef.put(e.target.files[0]).on(
+                              'state_changed',
+                              (snap) => {},
+                              (err) => {
+                                console.log(err, 'ERROR');
+                              },
+                              async () => {
+                                let url: any = await storageRef.getDownloadURL();
+                                db.collection('modules').doc(router.query.id).collection('data').add({
+                                  image: url,
+                                });
+                              },
+                            );
+                          }}
+                        />
+                      </InputGroup>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col breakPoint={{ xs: 12, sm: 8 }}>
+                  <Card size="Large" style={{ border: '2px solid rgb(237, 241, 247)', boxShadow: 'none' }}>
+                    <CardBody>
+                      <Row>
+                        {galleryImages.map((data, i): any => {
+                          return (
+                            <Col breakPoint={{ xs: 12, sm: 4 }} key={i} style={{ position: 'relative' }}>
+                              <div style={{ position: 'absolute', top: '10px', right: '20px' }}>
+                                <Button
+                                  status="Danger"
+                                  type="button"
+                                  style={{ fontSize: '9px', padding: '3px 6px' }}
+                                  onClick={() => {
+                                    db.collection('modules')
+                                      .doc(router.query.id)
+                                      .collection('data')
+                                      .doc(data.id)
+                                      .delete();
+                                  }}
+                                >
+                                  Delete
+                                </Button>
+                              </div>
+                              <img src={data?.image} style={{ maxWidth: '100%' }} />
+                            </Col>
+                          );
+                        })}
+                      </Row>
+                    </CardBody>
+                  </Card>
+                </Col>
               </Row>
-            </div>
-            <Row>
-              <Col breakPoint={{ xs: 12, sm: 4 }}>
-                <Card size="Large" style={{ border: '2px solid rgb(237, 241, 247)', boxShadow: 'none' }}>
-                  <CardBody>
-                    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                      <div
-                        style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
-                        onClick={() => router.push(router.asPath + '/new')}
-                      >
-                        <GrAddCircle style={{ marginRight: '6px' }} />
-                        Add
-                      </div>
-                    </div>
-                    <br />
-                    <Input name="name" placeholder="Gallery Title" />
-                    <List></List>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col breakPoint={{ xs: 12, sm: 8 }}>
-                <Card size="Large" style={{ border: '2px solid rgb(237, 241, 247)', boxShadow: 'none' }}>
-                  <CardBody></CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
-      </Form>
+            </>
+          </Form>
+        </CardBody>
+      </Card>
     </Layout>
   );
 };
